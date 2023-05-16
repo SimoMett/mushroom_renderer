a933d081b5b8fd0390d0e5a705038212774a7d32
DonSimonetti <matt.simonetti@hotmail.com>
2023-05-17 01:51:57 +0200

    Improved colorpickerbutton

--- src/colorpickerbutton.py
+++ src/colorpickerbutton.py
@@ -5,10 +5,10 @@ from tkcolorpicker import askcolor
 
 class ColorPickerButton:
 
-    def __init__(self, master):
+    def __init__(self, master, command=None):
         self.master = master
         self.current_color = 0
-
+        self.command = command
         self.img = ImageTk.PhotoImage(Image.new('RGB', (24, 24), self.current_color))
         self.button = ttk.Button(self.master, image=self.img, command=self.open_color_chooser)
         return
@@ -16,8 +16,10 @@ class ColorPickerButton:
     def open_color_chooser(self):
         color = askcolor((255, 255, 0), self.master)
         if color is not None:
-            self.current_color = color[0]
+            self.current_color = (color[0][2] << 16) + (color[0][1] << 8) + color[0][0]
             self.img = ImageTk.PhotoImage(Image.new('RGB', (24, 24), color=self.current_color))
             self.button.configure(image=self.img)
             self.button.photo = self.img
+        if self.command is not None:
+            self.command()
         return
