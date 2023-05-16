bebce91b934a8aa5270b1469425f037c6f2a9d0e
DonSimonetti <matt.simonetti@hotmail.com>
2023-05-17 00:40:00 +0200

    Extracted into a class

--- tests/colorpicker_test.py
+++ tests/colorpicker_test.py
@@ -6,11 +6,26 @@ from PIL import ImageTk, Image
 from tkcolorpicker import askcolor
 
 
-def open_color_chooser():
-    color = askcolor((255, 255, 0), root)
-    new_img = ImageTk.PhotoImage(Image.new('RGB', (16, 16), color=color[0]))
-    test_button.configure(image=new_img)
-    test_button.photo = new_img
+class ColorPickerButton:
+
+    def __init__(self, master):
+        self.current_color = 0
+
+        self.img = ImageTk.PhotoImage(Image.new('RGB', (24, 24), self.current_color))
+        self.button = ttk.Button(image_frame, image=self.img, command=self.open_color_chooser)
+        return
+
+    def pack(self):
+        self.button.pack()
+
+    def open_color_chooser(self):
+        color = askcolor((255, 255, 0), root)
+        if color is not None:
+            self.current_color = color[0]
+            self.img = ImageTk.PhotoImage(Image.new('RGB', (24, 24), color=self.current_color))
+            self.button.configure(image=self.img)
+            self.button.photo = self.img
+        return
 
 
 root = tk.Tk()
@@ -19,8 +34,7 @@ root.minsize(100, 100)
 
 image_frame = ttk.Frame(root)
 
-img = ImageTk.PhotoImage(Image.new('RGB', (16, 16), color='red'))
-test_button = ttk.Button(image_frame, text="test", image=img, command=open_color_chooser)
+test_button = ColorPickerButton(image_frame)
 test_button.pack()
 
 image_frame.pack()
