f3d8ae40e80ebcd6e9e7158a02bce7589b7b7d07
DonSimonetti <matt.simonetti@hotmail.com>
2023-05-17 00:44:02 +0200

    Extracted into a module

---
+++ src/colorpickerbutton.py
@@ -0,0 +1,26 @@
+import tkinter as ttk
+from PIL import ImageTk, Image
+from tkcolorpicker import askcolor
+
+
+class ColorPickerButton:
+
+    def __init__(self, master):
+        self.master = master
+        self.current_color = 0
+
+        self.img = ImageTk.PhotoImage(Image.new('RGB', (24, 24), self.current_color))
+        self.button = ttk.Button(self.master, image=self.img, command=self.open_color_chooser)
+        return
+
+    def pack(self):
+        self.button.pack()
+
+    def open_color_chooser(self):
+        color = askcolor((255, 255, 0), self.master)
+        if color is not None:
+            self.current_color = color[0]
+            self.img = ImageTk.PhotoImage(Image.new('RGB', (24, 24), color=self.current_color))
+            self.button.configure(image=self.img)
+            self.button.photo = self.img
+        return
--- tests/colorpicker_test.py
+++ tests/colorpicker_test.py
@@ -1,32 +1,7 @@
 import tkinter as tk
 import tkinter.ttk as ttk
 
-import PIL.Image
-from PIL import ImageTk, Image
-from tkcolorpicker import askcolor
-
-
-class ColorPickerButton:
-
-    def __init__(self, master):
-        self.current_color = 0
-
-        self.img = ImageTk.PhotoImage(Image.new('RGB', (24, 24), self.current_color))
-        self.button = ttk.Button(image_frame, image=self.img, command=self.open_color_chooser)
-        return
-
-    def pack(self):
-        self.button.pack()
-
-    def open_color_chooser(self):
-        color = askcolor((255, 255, 0), root)
-        if color is not None:
-            self.current_color = color[0]
-            self.img = ImageTk.PhotoImage(Image.new('RGB', (24, 24), color=self.current_color))
-            self.button.configure(image=self.img)
-            self.button.photo = self.img
-        return
-
+from src.colorpickerbutton import ColorPickerButton
 
 root = tk.Tk()
 root.geometry("100x100")
