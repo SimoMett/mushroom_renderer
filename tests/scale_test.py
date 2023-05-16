e5d65dad89be2ab2888106d94a2b726c578b790d
matteo <matt.simonetti@hotmail.com>
2023-05-16 18:23:36 +0200

    Creating canvas

--- tests/scale_test.py
+++ tests/scale_test.py
@@ -1,8 +1,19 @@
 from tkinter import *
+from tkinter import Canvas
+from PIL import Image, ImageTk
+from PIL.Image import Resampling
 
-root = Tk()
+def load_png(png_file: str):
+    png = Image.open(png_file)
+    square_size = 320
+    resized_image = png.resize((square_size, square_size), Resampling.NEAREST)
+    return ImageTk.PhotoImage(resized_image)
+
+
 min_height = 600
 min_width = 900
+root = Tk()
+root.title("MycologyMC Fungi builder")
 root.geometry(str(min_width)+"x"+str(min_height))
 root.minsize(min_width, min_height)
 
@@ -21,7 +32,14 @@ for name in template_names:
 scales_frame.grid(row=0, column=0)
 
 image_frame = LabelFrame(root)
-Label(image_frame, text="test").pack()
+
+#Label(image_frame, image=load_png("test.png")).pack()
+w = Canvas(image_frame, width=600, height=600)
+
+w.create_image(10, 10, anchor=NW, image=load_png("test.png"))
+w.pack(side="right")
+
 image_frame.grid(row=0, column=1)
+
 
 mainloop()
