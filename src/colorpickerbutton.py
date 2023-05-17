e0632a75cde28e632f7e8663c4c981ce24718531
matteo <matt.simonetti@hotmail.com>
2023-05-17 11:00:39 +0200

    Fixed picker not choosing previous color

--- src/colorpickerbutton.py
+++ src/colorpickerbutton.py
@@ -3,18 +3,21 @@ from PIL import ImageTk, Image
 from tkcolorpicker import askcolor
 
 
+def color_to_tuple(current_color):
+    return current_color & 0xff, (current_color & 0xff00) >> 8, (current_color & 0xff0000) >> 16
+
 class ColorPickerButton:
 
     def __init__(self, master, command=None):
         self.master = master
-        self.current_color = -1
+        self.current_color = 0xffffff
         self.command = command
         self.img = ImageTk.PhotoImage(Image.new('RGB', (24, 24), self.current_color))
         self.button = ttk.Button(self.master, image=self.img, command=self.open_color_chooser)
         return
 
     def open_color_chooser(self):
-        color = askcolor((255, 255, 0), self.master)
+        color = askcolor(color_to_tuple(self.current_color), self.master)
         if color is not None and color[0] is not None:
             self.current_color = (color[0][2] << 16) + (color[0][1] << 8) + color[0][0]
             self.img = ImageTk.PhotoImage(Image.new('RGB', (24, 24), color=self.current_color))
