aa4f203501b1f7baaf6e792ee80684d1ed152e41
DonSimonetti <matt.simonetti@hotmail.com>
2023-05-19 16:03:59 +0200

    Refactoring: Reimplemented copy to clipboard

--- mycology_fungi_builder.py
+++ mycology_fungi_builder.py
@@ -9,18 +9,18 @@ current_fungus_type = CRIMSON_FUNGUS_TYPE
 
 
 def scale_notify(val):
-    #update_gui()
+
     return
 
 
-def copy_colors_to_clipboard(as_hex=True):  # FIXME
+def copy_colors_to_clipboard(as_hex=True):
     if as_hex:
-        clipboard_string = str([hex(color_pickers[j].current_color) for j in range(4)]) \
+        clipboard_string = str([hex(color_pickers[j].color_picker_button.current_color) for j in range(4)]) \
             .removeprefix("[") \
             .removesuffix("]") \
             .replace("'", "")
     else:
-        clipboard_string = str([color_pickers[j].current_color for j in range(4)]).removeprefix("[").removesuffix("]")
+        clipboard_string = str([color_pickers[j].color_picker_button.current_color for j in range(4)]).removeprefix("[").removesuffix("]")
     pyperclip.copy(clipboard_string)
 
 
@@ -43,7 +43,7 @@ if __name__ == "__main__":
     # FIXME broken + HSV is better suited
     # TODO update mechanism on color changes
     template_names = ["Stelum", "Head", "Details", "Details2"]
-    labels = ["Red", "Green", "Blue"]
+    labels = ["Hue", "Saturation", "Brightness"]
     color_scales = []
     scales_frame = Frame(root)
     for name in template_names:
@@ -61,7 +61,6 @@ if __name__ == "__main__":
 
     ### build color pickers
     color_pickers = []
-    color_labels = []
     color_output_frame = LabelFrame(root)
 
     i = 0
@@ -82,10 +81,10 @@ if __name__ == "__main__":
     copy_colors_menu.add_command(label="Copy to clipboard as HEX", command=lambda: copy_colors_to_clipboard(True))
     copy_colors_menu.add_command(label="Copy to clipboard as INT", command=lambda: copy_colors_to_clipboard(False))
     color_output_frame.bind("<Button-3>", lambda evt: copy_colors_menu.post(evt.x_root, evt.y_root))
-    #for button in color_pickers:
-        #button.button.bind("<Button-3>", lambda evt: copy_colors_menu.post(evt.x_root, evt.y_root))
-    for label in color_labels:
-        label.bind("<Button-3>", lambda evt: copy_colors_menu.post(evt.x_root, evt.y_root))
+    for picker in color_pickers:
+        picker.color_picker_button.button.bind("<Button-3>", lambda evt: copy_colors_menu.post(evt.x_root, evt.y_root))
+        picker.color_label.bind("<Button-3>", lambda evt: copy_colors_menu.post(evt.x_root, evt.y_root))
+        picker.template_label.bind("<Button-3>", lambda evt: copy_colors_menu.post(evt.x_root, evt.y_root))
 
     root.bind("<Button-1>", lambda arg: image_frame.switch_template_menu.unpost())
 
