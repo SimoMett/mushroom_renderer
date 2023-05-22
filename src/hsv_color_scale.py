9d40f35bb3ee321677bce647dd19c82427910370
DonSimonetti <matt.simonetti@hotmail.com>
2023-05-22 14:15:42 +0200

    Added some methods

--- src/hsv_color_scale.py
+++ src/hsv_color_scale.py
@@ -1,16 +1,37 @@
+import colorsys
 from tkinter import Label, Scale, HORIZONTAL
+
+
+def rgb_to_hsv(rgb: tuple):
+    (r, g, b) = (rgb[0] / 255, rgb[1] / 255, rgb[2] / 255)
+    (h, s, v) = colorsys.rgb_to_hsv(r, g, b)
+    return int(h * 179), int(s * 255), int(v * 255)
+
+
+hsv_labels = ["Hue", "Saturation", "Brightness"]
 
 
 class HsvColorScale:
 
     def __init__(self, master, template_name):
-        labels = ["Hue", "Saturation", "Brightness"]
+        self.template_name = template_name
+        self.hsv_scales = [None, None, None]
         for i in range(3):
-            Label(master, text=labels[i % 3]).grid(row=i, column=0)
-            w2 = Scale(master, from_=0, to=255, orient=HORIZONTAL, length=360)
-            # w2 = Scale(frame, from_=0, to=255, orient=HORIZONTAL, length=360,
-            #           command=lambda val: scale_notify(name + "_" + labels[i % 3], val))
+            Label(master, text=hsv_labels[i]).grid(row=i, column=0)
+            self.hsv_scales[i] = Scale(master, from_=0, to=255, orient=HORIZONTAL, length=360,
+                                       command=lambda val: self.scale_notify(hsv_labels[i], val))
 
-            w2.set(127)
-            w2.grid(row=i, column=1)
+            self.hsv_scales[i].set(127)
+            self.hsv_scales[i].grid(row=i, column=1)
             # color_scales.update({template_name + "_" + labels[i % 3]: w2})
+
+    def change_color(self, color):
+        pass
+
+    def update_scales(self):
+        pass
+
+    def scale_notify(self, channel, val):
+        print(self.template_name + "_" + channel, val)
+        # TODO calculate rgb from hsv
+        # TODO update colors in buttons and figure
