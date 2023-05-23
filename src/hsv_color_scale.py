dad0137249dbb285135a7da483de99fcbbf4edc9
DonSimonetti <matt.simonetti@hotmail.com>
2023-05-23 16:00:01 +0200

    Fixed bug

--- src/hsv_color_scale.py
+++ src/hsv_color_scale.py
@@ -64,7 +64,7 @@ class HsvColorScale:
 
     def saturation_change(self, val):
         template_id = template_names.index(self.template_name)
-        hsv = (self.hsv_scales[1].get(), int(val), self.hsv_scales[2].get())
+        hsv = (self.hsv_scales[0].get(), int(val), self.hsv_scales[2].get())
         self.colors_data_model.change_color_hsv(template_id, hsv)
 
     def value_change(self, val):
