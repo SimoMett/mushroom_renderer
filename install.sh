88655002cf46406d83770a2df7142ae148c10b5a
DonSimonetti <matt.simonetti@hotmail.com>
2023-05-24 13:08:05 +0200

    Removed run.sh script presence

--- install.sh
+++ install.sh
@@ -1,3 +1,2 @@
 sudo pyinstaller mycology_fungi_builder.spec --noconfirm --distpath /opt
-sudo chmod +x /opt/mycology_fungi_builder/run.sh
-sudo ln -s /opt/mycology_fungi_builder/run.sh /usr/local/bin/mycology_fungi_builder
+sudo ln -s /opt/mycology_fungi_builder/mycology_fungi_builder /usr/local/bin/mycology_fungi_builder
