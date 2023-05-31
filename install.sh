e4fd4eb2880e59c6fa91a17284965fdaa8952c62
matteo <matt.simonetti@hotmail.com>
2023-05-31 14:58:32 +0200

    Removed sudo

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
-sudo pyinstaller mycology_fungi_builder.spec --noconfirm --distpath /opt
-sudo ln -s /opt/mycology_fungi_builder/mycology_fungi_builder /usr/local/bin/mycology_fungi_builder
+pyinstaller mycology_fungi_builder.spec --noconfirm --distpath /opt
+ln -s /opt/mycology_fungi_builder/mycology_fungi_builder /usr/local/bin/mycology_fungi_builder
