5c6856fa0a6e611e9e110339397392cba73024d7
SimoMett <matt.simonetti@hotmail.it>
2024-01-31 17:07:43 +0100

    Unlink previous install

--- install.sh
+++ install.sh
@@ -1,2 +1,4 @@
-pyinstaller mycology_fungi_builder.spec --noconfirm --distpath /opt
-ln -s /opt/mycology_fungi_builder/mycology_fungi_builder /usr/local/bin/mycology_fungi_builder
+sudo pyinstaller mycology_fungi_builder.spec --noconfirm --distpath /opt
+sudo unlink /usr/local/bin/mycology_fungi_builder
+sudo chmod +x /opt/mycology_fungi_builder/mycology_fungi_builder
+sudo ln -s /opt/mycology_fungi_builder/mycology_fungi_builder /usr/local/bin/mycology_fungi_builder
