226a80dec6b935e979a3438c61f62457f08f5b5e
SimoMett <matt.simonetti@hotmail.it>
2024-02-22 18:39:56 +0100

    Finished json_to_svg.py

--- json_to_svg.py
+++ json_to_svg.py
@@ -1,17 +1,22 @@
+import json
+
 import cv2
 import svg
 import sys
 
-from src.draw_fungus import CRIMSON_FUNGUS_TYPE, WARPED_FUNGUS_TYPE, draw_fungus
+from src.draw_fungus import CRIMSON_FUNGUS_TYPE, WARPED_FUNGUS_TYPE, draw_fungus, fungus_types
 
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("ERROR")
-        exit()
 
-    colors = [0xffffff, 0xffffff, 0, 0xffffff]
-    file_name = sys.argv[1].removesuffix(".json")
-    result = draw_fungus(colors, CRIMSON_FUNGUS_TYPE)
+def export_svg(file_name):
+    f = open(file_name, "r")
+    data = json.load(f)
+    colors = data["colors"]
+    fungus_type = str(data["type"])
+    fungus_type = fungus_type.removesuffix("_fungus")
+    fungus_type = fungus_type.removeprefix("colored_")
+    f.close()
+    file_name = file_name.removesuffix(".json")
+    result = draw_fungus(colors, fungus_types.index(fungus_type))
     result = cv2.cvtColor(result, cv2.COLOR_RGB2RGBA)
     _elements = []
     for i in range(16):
@@ -22,7 +27,6 @@ if __name__ == "__main__":
             box = svg.Rect(x=i, y=j, width=1, height=1,
                            fill=str(color).replace("0x", "#"))
             _elements.append(box)
-
     canvas = svg.SVG(
         width=16,
         height=16,
@@ -31,3 +35,8 @@ if __name__ == "__main__":
     f = open(file_name + ".svg", "w")
     f.write(canvas.__str__())
     f.close()
+
+
+if __name__ == "__main__":
+    for i in range(1, len(sys.argv)):
+        export_svg(sys.argv[i])
