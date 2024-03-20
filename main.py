86d83cb2949d598f47b290a7f3110c88f20f2398
matteo <matt.simonetti@hotmail.it>
2024-03-20 10:18:18 +0100

    Implemented all the color buttons

--- main.py
+++ main.py
@@ -15,10 +15,13 @@ from src.draw_fungus import draw_fungus, WARPED_FUNGUS_TYPE, CRIMSON_FUNGUS_TYPE
 
 class FungusModel:
     def __init__(self):
-        self.colors = [0xffffff for i in range(4)]
-        self.preview_colors = self.colors
+        self.__colors = [0xffffff for i in range(4)]
+        self.__editing_id = 0
         self.type = CRIMSON_FUNGUS_TYPE
         self.observers = []
+
+    def colors(self):
+        return self.__colors
 
     def subscribe(self, observer):
         self.observers.append(observer)
@@ -28,9 +31,6 @@ class FungusModel:
             obs.update_texture()
         return
 
-    def apply_preview_colors(self):
-        self.colors = self.preview_colors
-
     def switch_type(self):
         if self.type == CRIMSON_FUNGUS_TYPE:
             self.type = WARPED_FUNGUS_TYPE
@@ -39,24 +39,33 @@ class FungusModel:
         self.notify()
 
     def change_stelum_random(self):
-        self.colors[0] = random.randint(0, 0xffffff)
-        self.notify()
+        self.change_color(0, random.randint(0, 0xffffff))
 
     def change_head_random(self):
-        self.colors[1] = random.randint(0, 0xffffff)
-        self.notify()
+        self.change_color(1, random.randint(0, 0xffffff))
 
     def change_details1_random(self):
-        self.colors[2] = random.randint(0, 0xffffff)
-        self.notify()
+        self.change_color(2, random.randint(0, 0xffffff))
 
     def change_details2_random(self):
-        self.colors[3] = random.randint(0, 0xffffff)
-        self.notify()
+        self.change_color(3, random.randint(0, 0xffffff))
 
     def change_color_random(self):
-        self.colors = [random.randint(0, 0xffffff) for i in range(4)]
+        self.change_colors([random.randint(0, 0xffffff) for i in range(4)])
+
+    def change_color(self, _id, color):
+        self.__colors[_id] = color
         self.notify()
+
+    def change_colors(self, colors):
+        self.__colors = colors
+        self.notify()
+
+    def editing(self, _id):
+        self.__editing_id = _id
+
+    def get_editing_id(self):
+        return self.__editing_id
 
 
 fungus_model = FungusModel()
@@ -67,7 +76,7 @@ class FungusImage(Image):
         super(FungusImage, self).__init__(**kwargs)
         fungus_model.subscribe(self)
         texture = Texture.create(size=(16, 16), colorfmt="rgba")
-        source = draw_fungus(fungus_model.colors, fungus_model.type)
+        source = draw_fungus(fungus_model.colors(), fungus_model.type)
         texture.blit_buffer(np.flipud(source).tobytes(order="A"), bufferfmt="ubyte", colorfmt="rgba")
         self.texture = texture
         # size and display
@@ -80,7 +89,7 @@ class FungusImage(Image):
 
     def update_texture(self):
         texture = Texture.create(size=(16, 16), colorfmt="rgba")
-        source = draw_fungus(fungus_model.colors, fungus_model.type)
+        source = draw_fungus(fungus_model.colors(), fungus_model.type)
         texture.blit_buffer(np.flipud(source).tobytes(order="A"), bufferfmt="ubyte", colorfmt="rgba")
         self.texture = texture
         self.allow_stretch = True
@@ -94,21 +103,21 @@ class MainScreen(Screen):
         layout = BoxLayout(orientation='vertical')
 
         self.fungus_image = FungusImage()
-        fungus_model.subscribe(self.fungus_image)
         layout.add_widget(self.fungus_image)
 
         grid_layout = GridLayout(cols=2)
 
-        type_button = Button(text="Type")
-        type_button.bind(on_release=self.switch_fungus_type)
-        save_button = Button(text="Save")
-        stelum_button = Button(text='Stelum')
-        random_stelum_button = Button(text='Random stelum')
-        random_head_button = Button(text='Random head')
-        random_details1_button = Button(text='Random details1')
-        random_details2_button = Button(text='Random details2')
+        type_button, save_button = Button(text="Type"), Button(text="Save")
+        stelum_button, random_stelum_button = Button(text='Stelum'), Button(text='Random stelum')
+        head_button, random_head_button = Button(text='Head'), Button(text='Random head')
+        details1_button, random_details1_button = Button(text='Details1'), Button(text='Random details1')
+        details2_button, random_details2_button = Button(text='Details2'), Button(text='Random details2')
 
-        stelum_button.bind(on_release=self.changer)
+        type_button.bind(on_release=self.switch_fungus_type)
+        stelum_button.bind(on_release=self.open_colorpicker)
+        head_button.bind(on_release=self.open_colorpicker)
+        details1_button.bind(on_release=self.open_colorpicker)
+        details2_button.bind(on_release=self.open_colorpicker)
         random_stelum_button.bind(on_release=self.change_stelum_random)
         random_head_button.bind(on_release=self.change_head_random)
         random_details1_button.bind(on_release=self.change_details1_random)
@@ -118,11 +127,11 @@ class MainScreen(Screen):
         grid_layout.add_widget(save_button)
         grid_layout.add_widget(stelum_button)
         grid_layout.add_widget(random_stelum_button)
-        grid_layout.add_widget(Button(text='Head'))
+        grid_layout.add_widget(head_button)
         grid_layout.add_widget(random_head_button)
-        grid_layout.add_widget(Button(text='Details1'))
+        grid_layout.add_widget(details1_button)
         grid_layout.add_widget(random_details1_button)
-        grid_layout.add_widget(Button(text='Details2'))
+        grid_layout.add_widget(details2_button)
         grid_layout.add_widget(random_details2_button)
 
         layout.add_widget(grid_layout)
@@ -131,7 +140,12 @@ class MainScreen(Screen):
         layout.add_widget(random_all_button)
         self.add_widget(layout)
 
-    def changer(self, *args):
+    def open_colorpicker(self, instance):
+        names_list = ["Stelum", "Head", "Details1", "Details2"]
+        _id = names_list.index(instance.text)
+        assert 0 <= _id < 4
+        global fungus_model
+        fungus_model.editing(_id)
         self.manager.current = 'screen2'
 
     def switch_fungus_type(self, *args):
@@ -159,24 +173,25 @@ class MainScreen(Screen):
         fungus_model.change_color_random()
 
 
-class SecondScreen(Screen):
+class ColorPickerScreen(Screen):
 
     def __init__(self, **kwargs):
-        super(SecondScreen, self).__init__(**kwargs)
+        super(ColorPickerScreen, self).__init__(**kwargs)
 
         layout = BoxLayout(orientation='vertical')
         self.fungus_image = FungusImage()
-        fungus_model.subscribe(self.fungus_image)
         layout.add_widget(self.fungus_image)
 
-        colorpicker = ColorPicker()
-        layout.add_widget(colorpicker)
+        self.previous_colors = fungus_model.colors()
+        self.colorpicker = ColorPicker()
+        self.colorpicker.bind(color=self.on_color)
+        layout.add_widget(self.colorpicker)
 
         grid_layout = GridLayout(cols=2)
-        ok_button = Button(text="ok")
-        ok_button.bind(on_release=self.changer)
-        cancel_button = Button(text="cancel")
-        cancel_button.bind(on_release=self.changer)
+        ok_button = Button(text="Ok")
+        ok_button.bind(on_release=self.confirm)
+        cancel_button = Button(text="Cancel")
+        cancel_button.bind(on_release=self.rollback)
         grid_layout.add_widget(ok_button)
         grid_layout.add_widget(cancel_button)
 
@@ -184,8 +199,32 @@ class SecondScreen(Screen):
 
         self.add_widget(layout)
 
-    def changer(self, *args):
+    def confirm(self, *args):
         self.manager.current = 'screen1'
+
+    def rollback(self, *args):
+        # rollback to previous colors
+        global fungus_model
+        fungus_model.change_colors(self.previous_colors)
+        self.manager.current = 'screen1'
+
+    def on_enter(self, *args):
+        global fungus_model
+        self.previous_colors = fungus_model.colors().copy()
+
+        # update color picker
+        curr_col = self.previous_colors[fungus_model.get_editing_id()]
+
+        r = (curr_col >> 16) / 255.0
+        g = ((curr_col >> 8) & 0xff) / 255.0
+        b = (curr_col & 0xff) / 255.0
+        self.colorpicker.set_color([r, g, b, 1.0])
+
+    def on_color(self, instance, value):
+        # update image
+        color = (int(value[0] * 255) << 16) + (int(value[1] * 255) << 8) + int(value[2] * 255)
+        global fungus_model
+        fungus_model.change_color(fungus_model.get_editing_id(), color)
 
 
 class MycologyApp(App):
@@ -193,7 +232,7 @@ class MycologyApp(App):
     def build(self):
         screen_manager = ScreenManager(transition=NoTransition())
         screen1 = MainScreen(name='screen1')
-        screen2 = SecondScreen(name='screen2')
+        screen2 = ColorPickerScreen(name='screen2')
         screen_manager.add_widget(screen1)
         screen_manager.add_widget(screen2)
         return screen_manager
